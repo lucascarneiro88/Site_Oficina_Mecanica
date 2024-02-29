1ea9f21e747d0393871a3c3eaa135edcc7494e96
Lucas <carneirolucas88@gmail.com>
2024-02-29 12:48:12 -0300

    adcionando contato ao footer

--- src/Components/ContactComponents/Contact.jsx
+++ src/Components/ContactComponents/Contact.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import './contact.css'; // Importe seu arquivo CSS de estilos
+import React from "react";
+import "./contact.css";
 
 function Contact() {
-    const phoneNumber = 'SEU-NÚMERO-DE-TELEFONE'; // Substitua pelo número de telefone da oficina
-    const whatsappUrl = `https://wa.me/${phoneNumber}`;
+  const phoneNumber = "(48) 99634-3207";
+  const whatsappUrl = `https://wa.me/${phoneNumber}`;
 
-    const handleWhatsAppClick = () => {
-        window.open(whatsappUrl, '_blank');
-    };
+  const handleWhatsAppClick = () => {
+    window.open(whatsappUrl, "_blank");
+  };
 
-    return (
-        <div>
-            <button className="whatsapp-button" onClick={handleWhatsAppClick}>
-            <img src="https://www.designi.com.br/images/preview/12585402-m.jpg" alt="WhatsApp Icon" />
-                <span>Contato via WhatsApp</span>
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      <button className="whatsapp-button" onClick={handleWhatsAppClick}>
+        <img
+          src="https://www.designi.com.br/images/preview/12585402-m.jpg"
+          alt="WhatsApp Icon"
+        />
+        <span>Contato via WhatsApp</span>
+      </button>
+    </div>
+  );
 }
 
 export default Contact;
