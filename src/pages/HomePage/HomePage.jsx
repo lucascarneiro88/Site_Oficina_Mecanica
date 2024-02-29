24ec80024dc9b00608f433de3a179bfb28c93332
Lucas <carneirolucas88@gmail.com>
2024-02-29 13:00:15 -0300

    import css

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import NavBar from "../../Components/NavbarComponents/NavBar.jsx";
+import Welcome from "../../Components/WelcomeComponents/Welcome.jsx";
+import ServiceCard from "../../Components/ServiceCardComponents/ServiceCard.jsx";
+import servicesData from "../../data/servicesData.js";
+import AboutUs from "../../Components/AboutUsComponents/AboutUs.jsx";
+import Footer from "../../Components/FooterComponents/Footer.jsx";
+import Review from "../../Components/ReviewsComponents/Review.jsx";
+
+import "./homePage.css";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <NavBar />
+      <Welcome />
+      <div className="home" id="services">
+        <h1>Nossos Serviços</h1>
+        <div className="services">
+          {servicesData.map((service, index) => (
+            <ServiceCard
+              key={index}
+              title={service.title}
+              description={service.description}
+              imageUrl={service.imageUrl}
+            />
+          ))}
+        </div>
+        <div>
+          <AboutUs />
+        </div>
+        <div id="reviews">
+          <Review />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
