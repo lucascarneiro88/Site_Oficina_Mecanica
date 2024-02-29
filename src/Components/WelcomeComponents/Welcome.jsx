352ffbf69d198ae03c2b2c36b7c7b9ca1f101ba2
Lucas <carneirolucas88@gmail.com>
2024-02-29 12:57:47 -0300

    Adicionadas animações de entrada para o texto

--- src/Components/WelcomeComponents/Welcome.jsx
+++ src/Components/WelcomeComponents/Welcome.jsx
@@ -1,19 +1,49 @@
-import React from 'react';
-import Contact from '../ContactComponents/Contact';
-import './welcome.css'; // Import do arquivo CSS
+import React from "react";
+import { motion } from "framer-motion";
+
+import "./welcome.css";
 
 function Welcome() {
   return (
-    <div className="welcome-container"> {/* Adicione a classe welcome-container aqui */}
+    <div className="welcome-container">
       <div className="welcome-text">
-        <h2>Bem-vindo ao nosso site!</h2>
-        <p>Estamos felizes em tê-lo aqui. Explore nossos serviços e descubra mais sobre nós.</p>
-      </div>
-      <div>
-        < Contact />
+        <motion.h1
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+          style={{
+            fontSize: "70px",
+            fontWeight: "bold",
+            color: "#fff",
+            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)", 
+          }}
+        >
+          Mauro Mecânico
+        </motion.h1>
+        <motion.h2
+          initial={{ opacity: 0, x: 100 }} 
+          animate={{ opacity: 1, x: 0 }} 
+          transition={{ duration: 1, delay: 1 }} 
+          style={{ fontSize: "24px", color: "#fff" }}
+        >
+          Bem-vindo ao nosso site!
+        </motion.h2>
+        <motion.p
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }} 
+          transition={{ duration: 1, delay: 1.5 }} 
+          style={{ color: "#fff" }}
+        >
+          Estamos felizes em tê-lo aqui. Explore nossos serviços e descubra mais
+          sobre nós.
+        </motion.p>
       </div>
       <div className="welcome-background">
-        <img src="/img/ImgWelcome2.png.jpg" alt="imagem caminhões" className="welcome-image" />
+        <img
+          src="../img/welcome.jpg"
+          alt="imagem caminhões"
+          className="welcome-image"
+        />
       </div>
     </div>
   );
