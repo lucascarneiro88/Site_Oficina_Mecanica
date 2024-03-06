91572f59a8caf926f2db00519b5b56e4734aaaf0
Lucas <carneirolucas88@gmail.com>
2024-03-06 12:24:34 -0300

    adicionada uma div com a classe container-paragrafos

--- src/Components/AboutUsComponents/AboutUs.jsx
+++ src/Components/AboutUsComponents/AboutUs.jsx
@@ -4,29 +4,15 @@ import "./aboutUs.css";
 function AboutUs() {
   return (
     <div className="about-us" id="about">
-      <h2>Quem Somos</h2>
-      <p>
-        Bem-vindo à Mauro Mecânico, o destino número um para proprietários de
-        caminhões que valorizam qualidade, experiência e confiança. Com mais de
-        30 anos no setor, Mauro é uma lenda viva da mecânica de caminhões.
-      </p>
-      <p>
-        Desde os seus primeiros dias como aprendiz em uma pequena garagem até se
-        tornar um mestre em sua arte, Mauro sempre foi apaixonado por caminhões.
-        Sua jornada é uma história de dedicação incansável e um compromisso
-        inabalável com a excelência.
-      </p>
-      <p>
-        Aqui na Mauro Mecânico, não apenas oferecemos serviços de reparo e
-        manutenção de alta qualidade, mas também proporcionamos uma experiência
-        personalizada para cada cliente. Nossa missão é simples: manter os
-        caminhões dos nossos clientes funcionando com segurança e eficiência
-        máximas.
-      </p>
-      <p>
-        Confie em nós para cuidar do seu veículo comercial como se fosse nosso
-        próprio. Na Mauro Mecânico, o seu caminhão está em mãos confiáveis.
-      </p>
+     <div className="container-paragrafos">
+     <h2>Quem Somos</h2>
+        <p>
+           Mauro Mecânico, o destino número um para proprietários que valorizam qualidade, experiência e confiança. Com mais de 30 anos no setor, Mauro é uma lenda viva da mecânica de caminhões.
+        </p>
+        <p>
+           Confie em nós para cuidar do seu veículo  como se fosse nosso próprio. Aqui você está em mãos confiáveis.
+        </p>
+      </div>
     </div>
   );
 }
