4fc3a5dcddb927e1bc0d8c13b8e24aabeb57b958
Lucas <carneirolucas88@gmail.com>
2024-03-06 12:30:03 -0300

    Atualizado componente Review para exibir comentários de clientes.

--- src/Components/ReviewsComponents/Review.jsx
+++ src/Components/ReviewsComponents/Review.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './review.css';
+import React from "react";
+import "./review.css";
 
 function Review() {
   return (
@@ -7,15 +7,24 @@ function Review() {
       <h1>Comentários dos Clientes</h1>
       <div className="review">
         <h2>Cliente Satisfeito</h2>
-        <p>"Excelente serviço! A oficina resolveu meu problema rapidamente e com um preço justo. Recomendo!"</p>
+        <p>
+          "Excelente serviço! A oficina resolveu meu problema rapidamente e com
+          um preço justo. Recomendo!"
+        </p>
       </div>
       <div className="review">
         <h2>Cliente Feliz</h2>
-        <p>"Fui muito bem atendido na Mauro Mecânico. Profissionais competentes e serviço de qualidade."</p>
+        <p>
+          "Fui muito bem atendido na Mauro Mecânico. Profissionais competentes e
+          serviço de qualidade."
+        </p>
       </div>
       <div className="review">
         <h2>Cliente Fiel</h2>
-        <p>"Já sou cliente há anos e sempre fico satisfeito com o serviço prestado. Recomendo a todos!"</p>
+        <p>
+          "Já sou cliente há anos e sempre fico satisfeito com o serviço
+          prestado. Recomendo a todos!"
+        </p>
       </div>
     </div>
   );
