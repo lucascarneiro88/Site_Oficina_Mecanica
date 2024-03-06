e754ea5b91a7a285b0c1ddffa033e84a4c74e898
Lucas <carneirolucas88@gmail.com>
2024-03-06 12:26:38 -0300

    Atualizado estilo do rodapé (footer) para incluir uma seção de redes sociais e ajustes menores.

--- src/Components/FooterComponents/Footer.jsx
+++ src/Components/FooterComponents/Footer.jsx
@@ -1,33 +1,42 @@
-import React from 'react'
+import React from 'react';
 import './footer.css';
-
 
 function Footer() {
   return (
     <footer className="footer">
-    <div className="container">
+      <div className="container">
         <div className="footer-content">
-            <div className="footer-section">
-                <h3>Contato</h3>
-                <p>Telefone: (48) 99634-3207</p>
-                <p>Email: exemplo@example.com</p>
-            </div>
-            <div className="footer-section">
-                <h3>Horário de Funcionamento</h3>
-                <p>Segunda a Sexta: 08:00 - 18:00</p>
-             
-            </div>
-            <div className="footer-section">
-                <h3>Endereço</h3>
-                <p>Rua: Av. Dona Santina C. da Silva - limeira</p>
-                <p>Cidade: Biguaçu - SC</p>
-                <p>CEP: 88160-000</p>
-            </div>
+          <div className="redes-sociais-footer">
+            <a
+              href="https://www.instagram.com/mauro.figueira.319/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a
+              href="https://www.facebook.com/profile.php?id=100087134086815"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-facebook"></i>
+            </a>
+          </div>
+          <div className="footer-section contato">
+            <h3>Contato</h3>
+            <p>Telefone: (48) 99634-3207</p>
+            <p>Email: exemplo@example.com</p>
+          </div>
+          <div className="footer-section endereco">
+          <h3>Endereço</h3>
+          <p>Rua: Av. Dona Santina C. da Silva - Limeira</p>
+          <p>Cidade: Biguaçu - SC</p>
+          <p>CEP: 88160-000</p>
+          </div>
         </div>
-    </div>
-</footer>
-);
+      </div>
+    </footer>
+  );
 }
 
-
-export default Footer
+export default Footer;
