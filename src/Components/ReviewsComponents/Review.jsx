a5e41c2af0d82698d80def36df93d2eeee304997
Lucas <carneirolucas88@gmail.com>
2024-03-12 11:59:14 -0300

    Refatora componente Reviews

--- src/Components/ReviewsComponents/Review.jsx
+++ src/Components/ReviewsComponents/Review.jsx
@@ -1,31 +1,20 @@
 import React from "react";
+
+import reviewsData from "../../data/reviewsData";
 import "./review.css";
 
-function Review() {
+function Review(props) {
+  const reviews = props.reviews.map((review, index) => (
+    <div key={index} className="review">
+      <h2>{review.nome}</h2>
+      <p>{review.comentario}</p>
+    </div>
+  ));
+
   return (
     <div>
       <h1>Comentários dos Clientes</h1>
-      <div className="review">
-        <h2>Cliente Satisfeito</h2>
-        <p>
-          "Excelente serviço! A oficina resolveu meu problema rapidamente e com
-          um preço justo. Recomendo!"
-        </p>
-      </div>
-      <div className="review">
-        <h2>Cliente Feliz</h2>
-        <p>
-          "Fui muito bem atendido na Mauro Mecânico. Profissionais competentes e
-          serviço de qualidade."
-        </p>
-      </div>
-      <div className="review">
-        <h2>Cliente Fiel</h2>
-        <p>
-          "Já sou cliente há anos e sempre fico satisfeito com o serviço
-          prestado. Recomendo a todos!"
-        </p>
-      </div>
+      {reviews}
     </div>
   );
 }
