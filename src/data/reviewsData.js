aabb1a00981335a95d3d4c93335f883904f7b60a
Lucas <carneirolucas88@gmail.com>
2024-03-12 12:00:41 -0300

    Cria array reviewsData

---
+++ src/data/reviewsData.js
@@ -0,0 +1,25 @@
+const reviewsData = [
+    {
+      nome: "Lucas Carneiro",
+      comentario: "Atendimento e serviços de qualidade, muito honesto e correto, com preço justo."
+    },
+    {
+      nome: "Ricardo Karmann",
+      comentario: "Entendem do assunto, honestos.... Gostando do modo de trabalho. Ficou muito bom o serviço feito! Parabéns!"
+    },
+    {
+      nome: "Ulisses Vidal",
+      comentario: "Melhor mecânico que já conheci, super honesto e atende emergências no local. Recomendadissimo!!!"
+    },
+    {
+      nome: "Rosa Edite Guesser",
+      comentario: "Ótimo Atendimento"
+    },
+    {
+      nome: "José Roberio dos anjos",
+      comentario: "Honesto e bom preço recomendo"
+    }
+  ];
+  
+  export default reviewsData;
+  
