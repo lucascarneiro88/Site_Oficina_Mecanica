12edeeb2040ef2ba1cbba925ba78dfa606ace41a
Lucas <carneirolucas88@gmail.com>
2024-03-12 11:59:47 -0300

    Cria array logoCarouselData

---
+++ src/data/logoCarouselData.js
@@ -0,0 +1,45 @@
+const logoCarouselData = [
+    {
+      id: 1,
+      url: '/img/logos-carousel/logo-agrale.png',
+      alt: 'Logotipo Agrale',
+    },
+    {
+      id: 2,
+      url: '/img/logos-carousel/logo-daf.png',
+      alt: 'Logotipo DAF',
+    },
+    {
+      id: 3,
+      url: '/img/logos-carousel/logo-scania.png',
+      alt: 'Logotipo Scania',
+    },
+    {
+      id: 4,
+      url: '/img/logos-carousel/logo-volvo.png',
+      alt: 'Logotipo Volvo',
+    },
+    {
+      id: 5,
+      url: '/img/logos-carousel/logo-volkswagem.png',
+      alt: 'Logotipo Volkswagen',
+    },
+    {
+      id: 6,
+      url: '/img/logos-carousel/logo-man.png',
+      alt: 'Logotipo MAN',
+    },
+    {
+      id: 7,
+      url: '/img/logos-carousel/logo-mercedez.png',
+      alt: 'Logotipo Mercedes-Benz',
+    },
+    {
+      id: 8,
+      url: '/img/logos-carousel/logo-iveco.png',
+      alt: 'Logotipo Iveco',
+    },
+  ];
+  
+  export default logoCarouselData;
+  
