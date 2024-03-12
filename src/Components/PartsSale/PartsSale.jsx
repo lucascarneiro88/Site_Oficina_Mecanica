d777d35ec75068e3b10f788c946635613e248fd6
Lucas <carneirolucas88@gmail.com>
2024-03-12 11:58:25 -0300

    Cria componente para carousel vendas

---
+++ src/Components/PartsSale/PartsSale.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './partsSale.css';
+function PartsSale() {
+  return (
+    <div className='venda'>
+      <div className='venda-slide'>
+        <span>
+          Vendas de peças, entre em contato conosco.
+          (48) 996343207
+        </span>
+        
+      </div>
+    </div>
+  )
+}
+
+export default PartsSale;
