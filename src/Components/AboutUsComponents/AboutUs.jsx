a0bfbbd1d669304bc04b74bfb0f3d8c52f7a961a
Lucas <carneirolucas88@gmail.com>
2024-03-13 16:47:48 -0300

    Refatorando componente

--- src/Components/AboutUsComponents/AboutUs.jsx
+++ src/Components/AboutUsComponents/AboutUs.jsx
@@ -7,7 +7,7 @@ function AboutUs() {
      <div className="container-paragrafos">
      <h2>Quem Somos</h2>
         <p>
-           Mauro Mecânico, o destino número um para proprietários que valorizam qualidade, experiência e confiança. Com mais de 30 anos no setor, Mauro é uma lenda viva da mecânica de caminhões.
+           Mauro Mecânico, o destino número um para proprietários que valorizam qualidade, experiência e confiança. <br />Com mais de 30 anos no setor, Mauro é uma lenda viva da mecânica de caminhões.
         </p>
         <p>
            Confie em nós para cuidar do seu veículo  como se fosse nosso próprio. Aqui você está em mãos confiáveis.
