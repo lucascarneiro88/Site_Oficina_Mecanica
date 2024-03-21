862b4634449d0f721a6bdb8c005441a16ee38d09
Lucas <carneirolucas88@gmail.com>
2024-03-21 13:28:23 -0300

    refactor: reorganizar parágrafos na seção "Quem Somos"

--- src/Components/AboutUsComponents/AboutUs.jsx
+++ src/Components/AboutUsComponents/AboutUs.jsx
@@ -4,13 +4,18 @@ import "./aboutUs.css";
 function AboutUs() {
   return (
     <div className="about-us" id="about">
-     <div className="container-paragrafos">
-     <h2>Quem Somos</h2>
+      <div className="container-paragrafos">
+        <h2>Quem Somos</h2>
         <p>
-           Mauro Mecânico, o destino número um para proprietários que valorizam qualidade, experiência e confiança. <br />Com mais de 30 anos no setor, Mauro é uma lenda viva da mecânica de caminhões.
+          Com mais de 30 anos no setor, Mauro é uma lenda viva da mecânica de
+          caminhões.
         </p>
         <p>
-           Confie em nós para cuidar do seu veículo  como se fosse nosso próprio. Aqui você está em mãos confiáveis.
+          Mauro Mecânico, o destino número um para proprietários que valorizam
+          qualidade, experiência e confiança.
+        </p>
+        <p>
+          Confie em nós para cuidar do seu veículo como se fosse nosso próprio.
         </p>
       </div>
     </div>
